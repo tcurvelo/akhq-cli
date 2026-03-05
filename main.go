576e3f71e3b26439943cfad9f6ffe4dd8586c254
Thiago Curvelo <tcurvelo@gmail.com>
2026-03-04 22:15:13 -0300

    add cobra dependency, token auth support, and topic reading

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ type Config struct {
 	Cluster  string
 	User     string
 	Pass     string
+	Token    string
 	Insecure bool
 }
 
@@ -25,6 +26,7 @@ func getConfigFromEnv() Config {
 		Cluster:  os.Getenv("AKHQ_CLUSTER"),
 		User:     os.Getenv("AKHQ_USER"),
 		Pass:     os.Getenv("AKHQ_PASS"),
+		Token:    os.Getenv("AKHQ_TOKEN"),
 		Insecure: insecure,
 	}
 }
@@ -40,6 +42,35 @@ func getClient(conf Config) http.Client {
 		},
 	}
 	return *client
+}
+
+func readTopic(conf Config, client http.Client, topic string, token string) error {
+	url := fmt.Sprintf("%s/api/%s/topic/%s/data", conf.BaseURL, conf.Cluster, topic)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return fmt.Errorf("error creating request: %v ", err)
+	}
+	req.Header.Set("Cookie", "JWT="+token)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error reading topic: %v", err)
+	}
+	var data interface{}
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return fmt.Errorf("error parsing response: %v", err)
+	}
+	fmt.Println(data.([]interface{}))
+
+	//	for _, message := range data.([]interface{})["results"] {
+	//		fmt.Println(message)
+	//	}
+	//
+	// resp.Body.Close()
+	// fmt.Println(data)
+	return nil
 }
 
 func GetOffsets(conf Config, client http.Client, topic string, token string) {
@@ -68,9 +99,16 @@ func GetOffsets(conf Config, client http.Client, topic string, token string) {
 		// id := group["id"]
 		var lag uint
 		for _, partition := range group["offsets"].([]interface{}) {
-			lag += uint(partition.(map[string]interface{})["offsetLag"].(float64))
+			val := partition.(map[string]interface{})["offsetLag"]
+			if val == nil {
+				continue
+			}
+			if offsetLag, isFloat := val.(float64); isFloat {
+				lag += uint(offsetLag)
+			}
 		}
 		fmt.Println(lag)
+		// fmt.Printf("%s\n\n", group["activeTopics"])
 	}
 
 }
@@ -78,16 +116,21 @@ func GetOffsets(conf Config, client http.Client, topic string, token string) {
 func main() {
 	conf := getConfigFromEnv()
 	client := getClient(conf)
-	token, err := Authenticate(conf, client)
-	if err != nil {
-		fmt.Println("Error authenticating: ", err)
-		return
+	token := conf.Token
+	var err error
+	if conf.Token == "" {
+		token, err = Authenticate(conf, client)
+		if err != nil {
+			fmt.Println("Error authenticating: ", err)
+			return
+		}
 	}
 
 	flag.Parse()
 	args := flag.Args()
 	if len(args) < 1 {
-		fmt.Println("Topic is missing.")
+		fmt.Printf("AKHQ_TOKEN=%s", token)
+		// fmt.Println("Topic is missing.")
 		os.Exit(1)
 	}
 	GetOffsets(conf, client, args[0], token)
